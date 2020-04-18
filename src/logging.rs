1a2d226ddf4fd5d998f0e28f1ba2a7561eb7c5c9
jugeeya <jugeeya@live.com>
2020-04-18 11:57:27 -0700

    add HexDump impl

--- src/logging.rs
+++ src/logging.rs
@@ -1,3 +1,6 @@
+use core::fmt::Display;
+use std::fmt;
+
 extern "C" {
     fn skyline_tcp_send_raw(bytes: *const u8, usize: u64);
 }
@@ -21,4 +24,28 @@ pub fn log(message: &str) {
             ));
         }
     };
+}
+
+/**  
+    For dumping a struct to 8 bytes per row
+    Example usage:
+    let val = SomeStruct::new();
+    println!("Hexdump:\n {}", HexDump(&val));
+*/
+pub struct HexDump<'a, T: Sized>(pub &'a T);
+
+impl<'a, T> Display for HexDump<'a, T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let size = core::mem::size_of::<T>();
+        let ptr = self.0 as *const T as *const u8;
+        let slice = unsafe { core::slice::from_raw_parts(ptr, size) };
+        let hex_dump = slice.iter()
+            .map(|byte| format!("{:02X}", byte))
+            .collect::<Vec<_>>()
+            .chunks(8)
+            .map(|chunk| chunk.join(" "))
+            .collect::<Vec<_>>()
+            .join("\n");
+        write!(f, "{}", hex_dump)
+    }
 }
