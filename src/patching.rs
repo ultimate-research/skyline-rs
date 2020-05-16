b1cf86a744bf564ae4182234a6361e8c1f923756
jam1garner <8260240+jam1garner@users.noreply.github.com>
2020-05-16 13:26:07 -0400

    Add BranchBuilder to help replace B/BL instructions

--- src/patching.rs
+++ src/patching.rs
@@ -39,3 +39,118 @@ pub unsafe fn patch_data_from_text<T: Sized + Copy>(text_offset: *const u8, offs
 
     Ok(())
 }
+
+pub enum BranchType {
+    Branch,
+    BranchLink
+}
+
+pub struct BranchBuilder {
+    branch_type: BranchType,
+    offset: Option<usize>,
+    ptr: Option<*const ()>,
+    // TODO: add NRO support
+}
+
+impl BranchBuilder {
+    fn internal_new() -> Self {
+        Self {
+            branch_type: BranchType::Branch,
+            offset: None,
+            ptr: None
+        }
+    }
+
+    /// Create new branch builder for a `b` ARM instruction
+    pub fn branch() -> Self {
+        Self {
+            branch_type: BranchType::Branch,
+            ..BranchBuilder::internal_new()
+        }
+    }
+
+    /// Create new branch builder for a `bl` ARM instruction
+    pub fn branch_link() -> Self {
+        Self {
+            branch_type: BranchType::BranchLink,
+            ..BranchBuilder::internal_new()
+        }
+    }
+
+    /// Set the offset within the executable of the instruction to replace
+    pub fn branch_offset(mut self, offset: usize) -> Self {
+        self.offset = Some(offset);
+
+        self
+    }
+
+    /// Offset within the executable for the branch to jump to
+    pub fn branch_to_offset(mut self, offset: usize) -> Self {
+        unsafe {
+            self.ptr = Some(
+                 (getRegionAddress(Region::Text) as *const u8)
+                    .offset(offset as isize) as *const ()
+            );
+        }
+
+        self
+    }
+
+    /// Set a pointer for the branch to be jumped to. Must be within +/- 128 MiB of the given offset
+    pub fn branch_to_ptr<T>(mut self, ptr: *const T) -> Self {
+        self.ptr = Some(ptr as *const ());
+
+        self
+    }
+
+    ///
+    /// Replaces an instruction at the provided offset with a branch to the given pointer.
+    ///
+    /// # Panics
+    ///
+    /// Panics if an offset/ptr hasn't been provided or if the pointer is out of range of the
+    /// branch
+    #[track_caller]
+    pub fn replace(self) {
+        let offset = match self.offset {
+            Some(offset) => offset,
+            None => panic!("Offset is required to replace")
+        };
+
+        let instr_magic = match self.branch_type {
+            BranchType::Branch => 0b000101,
+            BranchType::BranchLink => 0b100101,
+        } << 26;
+
+        let branch_ptr = unsafe {
+            (getRegionAddress(Region::Text) as *const u8).offset(offset as isize)
+        } as isize;
+
+        let branch_to_ptr = match self.ptr {
+            Some(ptr) => ptr as *const u8,
+            None => panic!("Either branch_to_ptr or branch_to_offset is required to replace")
+        } as isize;
+
+        let imm26 = match (branch_to_ptr - branch_ptr) / 4 {
+            distance if within_branch_range(distance)
+                => ((branch_to_ptr - branch_ptr) as usize) >> 2,
+            _ => panic!("Branch target is out of range, must be within +/- 128 MiB")
+        };
+
+        let instr: u64 = (instr_magic | imm26) as u64;
+
+        unsafe {
+            if let Err(err) = patch_data(offset, &instr) {
+                panic!("Failed to patch data, error: {:?}", err)
+            }
+        }
+    }
+}
+
+#[allow(non_upper_case_globals)]
+const MiB: isize = 0x100000;
+const BRANCH_RANGE: isize = 128 * MiB;
+
+fn within_branch_range(distance: isize) -> bool {
+    (-BRANCH_RANGE..BRANCH_RANGE).contains(&distance)
+}
