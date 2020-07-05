e776f9856ed27f2dd35e829d7ed5db2b7d707aeb
jam1garner <8260240+jam1garner@users.noreply.github.com>
2020-07-05 12:07:55 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -50,7 +50,7 @@ pub fn c_str(string: &str) -> *const u8 {
 
 /// Helper to convert a C-str to a Rust string
 pub unsafe fn from_c_str(c_str: *const u8) -> String {
-    let name_slice = Vec::from_raw_parts(c_str as *mut _, strlen(c_str), strlen(c_str));
+    let name_slice = core::slice::from_raw_parts(c_str as *mut _, strlen(c_str));
     match core::str::from_utf8(&name_slice) {
         Ok(v) => v.to_owned(),
         Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
