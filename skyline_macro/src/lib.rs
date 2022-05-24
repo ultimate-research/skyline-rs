690874b28e94d2bbe76b3acdd22173ae65465f95
jam1garner <jam@jam1.re>
2022-05-24 17:01:13 -0400

    Revert "Disable warnings in original/call_original"

--- skyline_macro/src/lib.rs
+++ skyline_macro/src/lib.rs
@@ -174,7 +174,6 @@ pub fn hook(attrs: TokenStream, input: TokenStream) -> TokenStream {
     // allow original!
     if !attrs.inline {
         let orig_stmt: Stmt = parse_quote! {
-            #[allow(unused_macros)]
             macro_rules! original {
                 () => {
                     {
@@ -195,7 +194,6 @@ pub fn hook(attrs: TokenStream, input: TokenStream) -> TokenStream {
         };
         mod_fn.block.stmts.insert(0, orig_stmt);
         let orig_stmt: Stmt = parse_quote! {
-            #[allow(unused_macros)] 
             macro_rules! call_original {
                 ($($args:expr),* $(,)?) => {
                     original!()($($args),*)
